9d4ff34e1cba1a88e52e2eb2103bd69a519c9134
William Lemus <55531728+wlemusl@users.noreply.github.com>
2021-07-28 18:01:37 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import cx_Freeze
+import sys
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+executables = [cx_Freeze.Executable("alien_invasion.py", base=base)]
+bin_inc = 'C:\\Users\\willi\\anaconda3\\Lib\\site-packages\\pygame'
+
+cx_Freeze.setup(
+    name="alien invasion",
+    options={"build_exe": {"packages":["pygame"],
+                        "excludes":["tkinter"],
+                        "include_files":['images/', "ai_high_score.txt"],
+                        "bin_path_includes": bin_inc}},
+    executables = executables
+    )
+
+
+
